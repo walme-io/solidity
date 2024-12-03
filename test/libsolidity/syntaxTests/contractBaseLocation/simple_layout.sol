7f4eb69906b6269ea1a1155fc67fc7abf548841d
Matheus Aguiar <matheus.pit@gmail.com>
2024-12-03 11:58:46 -0300

    Add test

--- test/libsolidity/syntaxTests/contractBaseLocation/simple_layout.sol
+++ test/libsolidity/syntaxTests/contractBaseLocation/simple_layout.sol
@@ -2,3 +2,4 @@ contract A layout at 0x1234 {}
 contract B layout at 1024 {}
 contract C layout at "C" {}
 // ----
+// TypeError 76: (81-84): Contract storage base location must be in range of type uint256. Current type is literal_string "C"
